a30c3cedf1a4aa6b579a802a9863741fa666e32b
LeonardoLobas <leonardolobas2703@gmail.com>
2025-10-15 17:55:19 -0300

    add Contato entity and persistence configuration

---
+++ src/main/java/br/com/fiap/contatos/ContatoApp.java
@@ -0,0 +1,9 @@
+package br.com.fiap.contatos;
+
+import br.com.fiap.contatos.model.Contato;
+
+public class ContatoApp {
+    public static void main(String[] args) {
+        Contato contato = new Contato();
+    }
+}
